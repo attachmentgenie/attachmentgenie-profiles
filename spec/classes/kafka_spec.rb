e19e5d4f78ce32ae7685aac0e320010a757c1374
Bram Vogelaar <attachmentgenie@inuits.eu>
2016-08-31 14:34:17 +0200

    zookeeper_connect must be specified

--- spec/classes/kafka_spec.rb
+++ spec/classes/kafka_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 describe 'profiles::kafka' do
 
   context 'with defaults for all parameters' do
+    let(:params) { {:zookeeper_connect => ['zookeeper.foo.bar']} }
     it { should contain_class('profiles::kafka') }
   end
 end
