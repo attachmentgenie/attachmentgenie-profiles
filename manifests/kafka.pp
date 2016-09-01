5b8854b5e0a37c3a536ff24ddfbc6b0a5f27db3d
Bram Vogelaar <attachmentgenie@inuits.eu>
2016-09-01 10:56:21 +0200

    kafka should advertise the proper ip address

--- manifests/kafka.pp
+++ manifests/kafka.pp
@@ -17,14 +17,16 @@ class profiles::kafka (
     ensure => present,
   }
   class { '::kafka':
-    broker_id         => 1,
-    conf_dir          => '/etc/kafka/conf.dist',
-    install_java      => false,
-    install_service   => false,
-    manage_repo       => false,
-    package_name      => 'kafka-server',
-    service_name      => 'kafka-server',
-    zookeeper_connect => $zookeeper_connect,
+    advertised_hostname => $::ipaddress_enp0s8,
+    broker_id           => 1,
+    conf_dir            => '/etc/kafka/conf.dist',
+    install_java        => false,
+    install_service     => false,
+    manage_repo         => false,
+    package_name        => 'kafka-server',
+    service_name        => 'kafka-server',
+    hostname            => $::ipaddress_enp0s8,
+    zookeeper_connect   => $zookeeper_connect,
   }
 
   Yumrepo['cloudera-cdh5'] -> Package['kafka-server']
