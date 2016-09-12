4bf73ac76e42c2f3d806f01fd3381e7ff0f399b1
Bram Vogelaar <attachmentgenie@inuits.eu>
2016-09-12 15:08:44 +0200

    adding bare minimum collectd profile

--- manifests/collectd.pp
+++ manifests/collectd.pp
@@ -1,37 +1,13 @@
+# This class can be used install collectd
+#
+# @example when declaring the collectd class
+#  class { '::profiles::collectd': }
+#
+# @param minimum_version (Boolean)) Install this version or newer.
 class profiles::collectd (
-  $fqdnlookup                              = false,
-  $graphitehost                            = 'graphite-gold.europe.intranet',
-  $graphiteport                            = 2003,
-  $minimum_version                         = '5.4',
-  $package_ensure                          = undef,
-  $purge                                   = true,
-  $purge_config                            = true,
-  $recurse                                 = true,
-  $write_graphite                          = true,
+  $minimum_version = '5.5',
 ) {
-  # validate boolean params
-  validate_bool(
-    $fqdnlookup,
-    $purge,
-    $purge_config,
-    $recurse,
-    $write_graphite
-  )
-
-  # validate integer params
-  validate_integer(
-    $graphiteport,
-  )
-
-  # validate string parameters
-  validate_string($graphitehost)
-
   class { '::collectd':
-    fqdnlookup      => $fqdnlookup,
-    package_ensure  => $package_ensure,
-    purge           => $purge,
-    purge_config    => $purge_config,
-    recurse         => $recurse,
     minimum_version => $minimum_version,
   }
 }
