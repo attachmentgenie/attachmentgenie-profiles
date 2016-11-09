8ab52ca1da3beff51871b293fdd77bbf64a66b95
Bram (B.J.P.) Vogelaar <bram.vogelaar@ing.nl>
2016-11-09 14:05:56 +0100

    adding option to manage the upstream repo

--- manifests/collectd.pp
+++ manifests/collectd.pp
@@ -3,14 +3,18 @@
 # @example when declaring the collectd class
 #  class { '::profiles::collectd': }
 #
+# @param additional_packages (Hash) Extra packages to install to satisfy plugin requirements
+# @param manage_repo (Boolean) Configure upstream rpm repo.
 # @param minimum_version (Boolean)) Install this version or newer.
 # @param plugins (Hash) List of plugin to install and their settings.
 class profiles::collectd (
   $additional_packages = {},
+  $manage_repo         = true,
   $minimum_version     = '5.5',
   $plugins             = {},
 ) {
   class { '::collectd':
+    manage_repo     => $manage_repo,
     minimum_version => $minimum_version,
   }
   create_resources( 'package', $additional_packages, { tag => 'collectd' })
