c55f34a850b5417199a5ef2b603fbbe3510e8b2d
Bram Vogelaar <bram@attachmentgenie.com>
2016-11-09 12:56:32 +0100

    adding option to install additional packages

--- manifests/collectd.pp
+++ manifests/collectd.pp
@@ -6,11 +6,14 @@
 # @param minimum_version (Boolean)) Install this version or newer.
 # @param plugins (Hash) List of plugin to install and their settings.
 class profiles::collectd (
-  $minimum_version = '5.5',
-  $plugins = {},
+  $additional_packages = {},
+  $minimum_version     = '5.5',
+  $plugins             = {},
 ) {
   class { '::collectd':
     minimum_version => $minimum_version,
   }
-  create_resources( 'class', $plugins)
+  create_resources( 'package', $additional_packages, { tag => 'collectd' })
+  create_resources( 'class', $plugins, { tag => 'collectd' })
+  Package <| tag == collectd |> -> Class <| tag == collectd |>
 }
