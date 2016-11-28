e273ffeffa7d5619d346de2f335483b6978d78a6
Bram Vogelaar <bram@attachmentgenie.com>
2016-11-28 21:56:08 +0100

    setting root_home fact

--- spec/classes/mysql_spec.rb
+++ spec/classes/mysql_spec.rb
@@ -2,7 +2,11 @@ require 'spec_helper'
 describe 'profiles::mysql' do
   on_os_under_test.each do |os, facts|
     context "on #{os}" do
-      let(:facts) { facts }
+      let(:facts) {
+        facts.merge({
+          :root_home => '/root',
+        })
+      }
       context 'with defaults for all parameters' do
         it { should contain_class('profiles::mysql') }
       end
