c6c2ee40f627666276b3d2339203c3940da54028
Bram Vogelaar <bram.vogelaar@ing.com>
2016-12-08 17:09:18 +0100

    adding baisic parameters to install flink as an archive

--- manifests/flink.pp
+++ manifests/flink.pp
@@ -3,6 +3,18 @@
 # @example when declaring the node role
 #  class { '::profiles::flink': }
 #
-class profiles::flink {
-  class { '::flink': }
+# @param archive_source (String) Flink Download url.
+# @param install_method (String) How to install flink.
+class profiles::flink (
+  $archive_source = 'http://apache.xl-mirror.nl/flink/flink-1.1.3/flink-1.1.3-bin-hadoop27-scala_2.11.tgz',
+  $install_method = 'package',
+){
+  validate_string(
+    $archive_source,
+    $install_method,
+  )
+  class { '::flink':
+    archive_source => $archive_source,
+    install_method => $install_method,
+  }
 }
