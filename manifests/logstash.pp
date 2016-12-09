8fcb89457a3b18cf43121ddeba4925e6bd0e4f9a
Bram Vogelaar <bram.vogelaar@ing.com>
2016-12-09 12:15:16 +0100

    allow a version to be ensured

--- manifests/logstash.pp
+++ manifests/logstash.pp
@@ -3,18 +3,24 @@
 # @example when declaring the logstash class
 #  class { '::profiles::logstash': }
 #
-# @param config_files (Hash)) content for logstash input, filters and output.
-# @param manage_repo (Boolean)) Setup repository to install logstash from.
-# @param repo_version (String)) Version family to install from.
+# @param config_files (Hash) content for logstash input, filters and output.
+# @param ensure (String) Which version of logstash to install.
+# @param manage_repo (Boolean) Setup repository to install logstash from.
+# @param repo_version (String) Version family to install from.
 class profiles::logstash (
   $config_files = {},
+  $ensure       = present,
   $manage_repo  = false,
   $repo_version = '2.4',
 ){
   validate_bool(
     $manage_repo,
   )
+  validate_string(
+    $ensure,
+  )
   class { '::logstash':
+    ensure         => $ensure,
     logstash_group => 'root',
     logstash_user  => 'root',
     manage_repo    => $manage_repo,
