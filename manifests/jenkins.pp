f47a32f94ee918aa961c1673c7a4a2f234520772
Bram Vogelaar <bram@inuits.eu>
2017-01-16 16:50:40 +0100

    adding option to set jenkins config, defaulting to localhost only, manage_repo switch, purging unmanaged plugins by default

--- manifests/jenkins.pp
+++ manifests/jenkins.pp
@@ -3,13 +3,31 @@
 # @example when declaring the jenkins class
 #  class { '::profiles::jenkins': }
 #
+# @param config_hash (Hash) Config settings
+# @param manage_repo (Boolean) Manage rpm repository.
 # @param plugins (Hash) Plugins to install.
+# @param purge_plugins (Boolean) Remove unmanaged plugins.
 class profiles::jenkins (
-  $plugins = {},
+  $config_hash   = { 'JENKINS_LISTEN_ADDRESS' => { 'value' => '127.0.0.1' }},
+  $manage_repo   = false,
+  $plugins       = {},
+  $purge_plugins = true,
 ) {
+  validate_bool(
+    $manage_repo,
+    $purge_plugins,
+  )
+  validate_hash(
+    $config_hash,
+    $plugins,
+  )
+
   class { '::jenkins':
+    config_hash        => $config_hash,
     configure_firewall => false,
     install_java       => false,
+    purge_plugins      => $purge_plugins,
+    repo               => $manage_repo,
   }
   create_resources(::jenkins::plugin, $plugins)
 }
