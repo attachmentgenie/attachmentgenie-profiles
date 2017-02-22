6c5abcc1063ebd654d08d7d717a61409d6f544b1
Bram Vogelaar <bram@inuits.eu>
2017-02-22 15:54:41 +0100

    adding parameters required to run enterprise

--- manifests/mongodb.pp
+++ manifests/mongodb.pp
@@ -3,11 +3,19 @@
 # @example when declaring the influxdb class
 #  class { '::profiles::influxdb': }
 #
+# @param client_package_name (String) client package to install.
 # @param manage_package_repo (Boolean) Manage repository.
+# @mongos_package_name (String) mongos package to install.
+# @param server_package_name (String) Package to install.
+# @param use_enterprise_repo (Boolean)
 # @param version (String) Version to install.
 class profiles::mongodb (
-  $manage_package_repo  = false,
-  $version              = '3.4.0',
+  $client_package_name = undef,
+  $manage_package_repo = false,
+  $mongos_package_name = undef,
+  $server_package_name = undef,
+  $use_enterprise_repo = false,
+  $version             = '3.4.2',
 ) {
   validate_bool(
     $manage_package_repo,
@@ -16,7 +24,11 @@ class profiles::mongodb (
     $version,
   )
   class { '::mongodb::globals':
+    client_package_name => $client_package_name,
     manage_package_repo => $manage_package_repo,
+    mongos_package_name => $mongos_package_name,
+    server_package_name => $server_package_name,
+    use_enterprise_repo => $use_enterprise_repo,
     version             => $version,
   } ->
   class { '::mongodb::server': }
