a96506611f42bc825b49c02a31c80c9ca37c447c
Bram Vogelaar <bram@attachmentgenie.com>
2017-02-22 11:59:48 +0100

    adding missing fact

--- spec/classes/selinux_spec.rb
+++ spec/classes/selinux_spec.rb
@@ -2,7 +2,11 @@ require 'spec_helper'
 describe 'profiles::selinux' do
   on_os_under_test.each do |os, facts|
     context "on #{os}" do
-      let(:facts) { facts }
+      let(:facts) do
+        facts.merge({
+          :selinux_agent_vardir => '/var/lib/puppet',
+        })
+      end
       context 'with defaults for all parameters' do
         it { should contain_class('profiles::selinux') }
       end
