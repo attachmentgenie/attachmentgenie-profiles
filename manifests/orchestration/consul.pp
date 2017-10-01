af83df345a43f96156304e1ec73a32683991c122
Bram Vogelaar <attachmentgenie@inuits.eu>
2017-10-01 22:22:33 +0200

    adding consul checks, services and watch

--- manifests/orchestration/consul.pp
+++ manifests/orchestration/consul.pp
@@ -3,16 +3,22 @@
 # @example when declaring the consul class
 #  class { '::profiles::orchestration::consul': }
 #
-# @param config  Consul config,
-# @param options Additional consul start up flags.
-# @param version Version of consul to install,
+# @param checks   Consul checks,
+# @param config   Consul config,
+# @param options  Additional consul start up flags.
+# @param services Consul services.
+# @param version  Version of consul to install.
+# @param watches  Consul watches.
 class profiles::orchestration::consul (
+  Hash $checks    = {},
   Hash $config    = {
     'data_dir'   => '/opt/consul',
-    'datacenter' => 'vagrant1',
+    'datacenter' => 'vagrant',
   },
   String $options = '-enable-script-checks -syslog',
+  Hash $services  = {},
   String $version = '0.9.3',
+  Hash $watches   = {},
 ) {
   package { 'unzip':
     ensure => present,
@@ -22,4 +28,8 @@ class profiles::orchestration::consul (
     extra_options => $options,
     version       => $version,
   }
+
+  create_resources(::consul::check, $checks)
+  create_resources(::consul::service, $services)
+  create_resources(::consul::watch, $watches)
 }
