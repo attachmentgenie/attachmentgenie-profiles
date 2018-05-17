e94872ac37fe6bdd214932b929b4bbc0a4b7d414
bjanssens <bjanssens@inuits.eu>
2018-05-17 10:56:41 +0200

    Feature: Icinga dashboard
    Signed-off-by: bjanssens <bjanssens@inuits.eu>

---
+++ manifests/alerting/icingaweb2/dashboard.pp
@@ -0,0 +1,17 @@
+# This class manages the icinga api user for the dashboard
+#
+#
+class profiles::alerting::icingaweb2::dashboard (
+  String $api_username = 'icingadashboard',
+  String $api_password = 'changemeplease!',
+) inherits profiles::alerting::icingaweb2 {
+
+  if ( $api_password == 'changemeplease!' ) {
+    notify { 'Using the default password, only use in development environments!': }
+  }
+
+  ::icinga2::object::apiuser { $api_username:
+    password    => $api_password,
+    permissions => [ "status/query", "objects/query/*" ],
+  }
+}
