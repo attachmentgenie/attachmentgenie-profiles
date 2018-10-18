dec0010f441177d40a8abe05915eb3c38822b999
Marc Lambrichs <mlambrichs@melange-it.nl>
2018-10-18 14:26:15 +0200

    Update profile kibana

--- manifests/logging/kibana.pp
+++ manifests/logging/kibana.pp
@@ -3,17 +3,23 @@
 # @example when declaring the kibana class
 #  class { '::profiles::logging::kibana': }
 #
-# @param manage_repo  Let profile install java.
-# @param repo_version Package repo family to install from.
+# @param config       Hash of key-value pairs for Kibana's configuration file
+# @param oss          Whether to manage OSS packages
+# @param manage_repo  Whether to manage the package manager repository.
+# @param status       Service status.
 # @param version      Version to install.
 class profiles::logging::kibana (
-  Boolean $manage_repo = false,
-  String $repo_version = '5.x',
-  String $version = present,
+  Hash[String[1], Variant[String[1], Integer, Boolean, Array]] $config,
+  Boolean $oss                                             = false,
+  Boolean $manage_repo                                     = false,
+  Enum['disabled','enabled','running','unmanaged'] $status = undef,
+  String $version                                          = present,
 ) {
   class { '::kibana':
-    ensure       => $version,
-    manage_repo  => $manage_repo,
-    repo_version => $repo_version,
+    ensure      => $version,
+    config      => $config,
+    manage_repo => $manage_repo,
+    status      => $status,
+    oss         => $oss
   }
 }
