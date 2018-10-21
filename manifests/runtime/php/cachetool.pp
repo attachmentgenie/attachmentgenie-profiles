f1c4d99694c3f7b6392d25caf4b6338f24fade10
Bram Vogelaar <attachmentgenie@inuits.eu>
2018-10-21 22:08:43 +0200

    only use new core modules where appropriate

--- manifests/runtime/php/cachetool.pp
+++ manifests/runtime/php/cachetool.pp
@@ -7,22 +7,22 @@
 # @param install_dir      Install directory
 # @param install_method   Install type.
 # @param package_name     Package name.
+# @param package_version  Version to be installed.
 # @param phar_source      Download source.
 # @param user             Owner of the file.
-# @param version          Version to be installed.
 class profiles::runtime::php::cachetool (
   Optional[String] $group   = undef,
   Stdlib::Absolutepath $install_dir = '/usr/local/bin',
   Profiles::InstallMethod $install_method = 'archive',
   String $package_name = 'cachetool',
+  String $package_version = 'installed',
   String $phar_source = 'http://gordalina.github.io/cachetool/downloads/cachetool.phar',
   String $user = 'root',
-  Optional[String] $version = undef,
 ) {
   case $install_method {
     'package': {
       package { 'cachetool':
-        ensure => $version,
+        ensure => $package_version,
         name   => $package_name,
       }
     }
---
+++ spec/classes/runtime/php/cachetool_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+describe 'profiles::runtime::php::cachetool' do
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) { facts }
+
+      context 'with defaults for all parameters' do
+        it { is_expected.to compile }
+      end
+
+      context 'install' do
+        context 'with install_method set to package' do
+          let(:params) do
+            {
+                install_method: 'package',
+                package_name: 'cachetool',
+            }
+          end
+
+          it { is_expected.to contain_package('cachetool') }
+        end
+
+        context 'with package_name set to specialpackage' do
+          let(:params) do
+            {
+                install_method: 'package',
+                package_name: 'specialpackage',
+            }
+          end
+
+          it { is_expected.to contain_package('cachetool').with_name('specialpackage') }
+        end
+
+        context 'with package_version set to 42.42.42' do
+          let(:params) do
+            {
+                install_method: 'package',
+                package_name: 'cachetool',
+                package_version: '42.42.42',
+            }
+          end
+
+          it { is_expected.to contain_package('cachetool').with_ensure('42.42.42') }
+        end
+      end
+    end
+  end
+end
