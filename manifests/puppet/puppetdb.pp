e6d1b4467c8409a300120d1f937786853ecd1f85
Bram Vogelaar <attachmentgenie@inuits.eu>
2019-06-21 17:41:37 +0200

    client tools were redeveloped and released as a gem

--- manifests/puppet/puppetdb.pp
+++ manifests/puppet/puppetdb.pp
@@ -11,6 +11,7 @@
 class profiles::puppet::puppetdb (
   String $database_host = 'localhost',
   String $database_password = 'puppetdb',
+  Boolean $install_client_tools = true,
   String $listen_address = '0.0.0.0',
   Boolean $manage_firewall = false,
   String $ssl_listen_address = '0.0.0.0',
@@ -23,9 +24,17 @@ class profiles::puppet::puppetdb (
     ssl_listen_address => $ssl_listen_address,
   }
 
-  if versioncmp($::puppetversion, '6.0.0') <= 0 {
-    package { 'puppet-client-tools':
-      ensure => installed,
+  if $install_client_tools {
+    if versioncmp($::puppetversion, '6.0.0') >= 0 {
+      package { 'puppetdb_cli':
+        ensure          => installed,
+        install_options => ['--bindir', '/opt/puppetlabs/bin'],
+        provider        => puppet_gem,
+      }
+    } else {
+      package { 'puppet-client-tools':
+        ensure => installed,
+      }
     }
   }
 
