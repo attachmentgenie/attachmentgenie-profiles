cfc595cd50d148cf10295d1722f8342e722c73ae
Bram Vogelaar <attachmentgenie@inuits.eu>
2020-03-16 23:31:42 +0100

    parameterize password encryption for those apps that cant deal with it

--- manifests/alerting/icingaweb2.pp
+++ manifests/alerting/icingaweb2.pp
@@ -77,9 +77,10 @@ class profiles::alerting::icingaweb2 (
 
   if $manage_database {
     profiles::database::postgresql::db { $database_name:
-      grant    => $database_grant,
-      password => $database_password,
-      user     => $database_user,
+      encrypted => false,
+      grant     => $database_grant,
+      password  => $database_password,
+      user      => $database_user,
     }
   }
   if $manage_sd_service {
--- manifests/database/postgresql/db.pp
+++ manifests/database/postgresql/db.pp
@@ -4,14 +4,20 @@
 #  class { '::profiles::database::postgresql::db': }
 #
 define profiles::database::postgresql::db (
-  String $user,
-  String $password,
+  Boolean $encrypted = true,
   String $grant = 'ALL',
+  String $password,
+  String $user,
 ) {
 
+  if $encrypted {
+    $_password = postgresql_password($user, $password)
+  } else {
+    $_password = $password
+  }
   ::postgresql::server::db { $title:
     user     => $user,
-    password => postgresql_password($user, $password),
+    password => $_password,
     grant    => $grant,
   }
 }
