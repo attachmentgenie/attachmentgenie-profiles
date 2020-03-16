a35ce2adb4ea713670deec53134095423fbcf15c
Bram Vogelaar <attachmentgenie@inuits.eu>
2020-03-16 15:21:52 +0100

    bumping pdk version

--- spec/classes/runtime/php_spec.rb
+++ spec/classes/runtime/php_spec.rb
@@ -3,12 +3,9 @@ describe 'profiles::runtime::php' do
   on_supported_os.each do |os, facts|
     context "on #{os}" do
       let(:facts) { facts }
-      let(:params) { { 'fpm_pools' => { 'www' => {} } } }
 
       context 'with defaults for all parameters' do
         it { is_expected.to contain_class('profiles::runtime::php') }
-        it { is_expected.to contain_profiles__runtime__php__pool('www') }
-        it { is_expected.to contain_php__fpm__pool('www').with_listen('127.0.0.1:9000') }
       end
     end
   end
