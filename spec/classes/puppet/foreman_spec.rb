f23d4c1ae3d88c7b00d99de536f18a6a99185d87
Bram Vogelaar <attachmentgenie@inuits.eu>
2020-03-17 08:43:46 +0100

    adding missing precondition now that we are moving defined types back into profiles

--- spec/classes/puppet/foreman_spec.rb
+++ spec/classes/puppet/foreman_spec.rb
@@ -3,6 +3,7 @@ describe 'profiles::puppet::foreman' do
   on_supported_os.each do |os, facts|
     context "on #{os}" do
       let(:facts) { facts }
+      let(:pre_condition) { 'include ::profiles::database::postgresql' }
 
       context 'with defaults for all parameters' do
         it { is_expected.to contain_class('profiles::puppet::foreman') }
