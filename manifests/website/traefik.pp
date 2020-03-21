05a936c5bdb128917c26bb7334d53350904d3342
Bram Vogelaar <attachmentgenie@inuits.eu>
2020-03-21 13:47:32 +0100

    limiting cipher set

--- manifests/website/traefik.pp
+++ manifests/website/traefik.pp
@@ -25,17 +25,9 @@ class profiles::website::traefik (
   String $sd_service_name = 'traefik',
   Array $sd_service_tags = ['metrics'],
   Array $tls_cipher_suites = [
-    "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256",
-    "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256",
     "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256", # TLS 1.2
-    "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
     "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384", # TLS 1.2
-    "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
-    "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305",
-    "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305",
-    "TLS_AES_128_GCM_SHA256",
-    "TLS_AES_256_GCM_SHA384",
-    "TLS_CHACHA20_POLY1305_SHA256",
+    "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256", # TLS 1.2
     "TLS_FALLBACK_SCSV"
   ],
   Enum['VersionTLS12','VersionTLS13'] $tls_min_version = 'VersionTLS12',
