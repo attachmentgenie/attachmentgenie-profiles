01e87c1f560bdd9ee4c5c12ecff3dde096489cab
Bram Vogelaar <attachmentgenie@inuits.eu>
2020-03-21 17:10:26 +0100

    cleaning up ciphers

--- manifests/website/traefik.pp
+++ manifests/website/traefik.pp
@@ -25,12 +25,9 @@ class profiles::website::traefik (
   String $sd_service_name = 'traefik',
   Array $sd_service_tags = ['metrics'],
   Array $tls_cipher_suites = [
-    "TLS_AES_256_GCM_SHA384",                      # TLS 1.3
     "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",       # TLS 1.2
-    "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305",        # TLS 1.2
     "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256", # TLS 1.2
     "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",       # TLS 1.2
-    "TLS_CHACHA20_POLY1305_SHA256",
     "TLS_FALLBACK_SCSV"
   ],
   Enum['VersionTLS12','VersionTLS13'] $tls_min_version = 'VersionTLS12',
