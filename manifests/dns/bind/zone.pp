882a14bdf52c26c649b3e06ad81b4d697f1c2185
Bram Vogelaar <bram@attachmentgenie.com>
2020-06-27 23:51:30 +0200

    fixing chicken and egg problem

--- manifests/dns/bind/zone.pp
+++ manifests/dns/bind/zone.pp
@@ -3,6 +3,10 @@
 # @example when declaring the bind class
 #  ::profiles::dns::bind::zone { 'foo.bar': }
 #
-define profiles::dns::bind::zone () {
-  ::dns::zone { $name: }
+define profiles::dns::bind::zone (
+  Optional[Stdlib::IP::Address::V4] $soaip = undef,
+) {
+  ::dns::zone { $name:
+    soaip => $soaip,
+  }
 }
