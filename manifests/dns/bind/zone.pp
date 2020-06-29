2a47a0076caa790813a3394a37e65b3ffa040933
Bram Vogelaar <bram@attachmentgenie.com>
2020-06-30 00:03:21 +0200

    allow update policy to be set

--- manifests/dns/bind/zone.pp
+++ manifests/dns/bind/zone.pp
@@ -7,10 +7,12 @@ define profiles::dns::bind::zone (
   Boolean $reverse = false,
   String $soa = $::fqdn,
   Optional[Stdlib::IP::Address::V4] $soaip = undef,
+  Optional[Dns::UpdatePolicy] $update_policy = undef,
 ) {
   ::dns::zone { $name:
-    reverse => $reverse,
-    soa     => $soa,
-    soaip   => $soaip,
+    reverse       => $reverse,
+    soa           => $soa,
+    soaip         => $soaip,
+    update_policy => $update_policy,
   }
 }
