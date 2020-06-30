64839aa705e6d891a80ae26a4b7b8f351fecd80b
Bram Vogelaar <bram@attachmentgenie.com>
2020-06-30 22:32:23 +0200

    starting migration to traefik2

---
+++ manifests/website/traefik2.pp
@@ -0,0 +1,61 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include profiles::website::traefik2
+class profiles::website::traefik2 (
+  Hash $dynamic_config = {},
+  Boolean $expose_api = true,
+  Boolean $expose_metrics = true,
+  Boolean $expose_ui = false,
+  Boolean $manage_firewall_entry = true,
+  Boolean $manage_sd_service = false,
+  Enum['http','https'] $protocol = 'https',
+  String $sd_service_name = 'traefik',
+  Array $sd_service_tags = ['metrics'],
+  Hash $static_config = {},
+  String $version = '2.2.1',
+) {
+  if $protocol == 'https' {
+    $firewall_ports = [80,443]
+  } else {
+    $firewall_ports = [80]
+  }
+
+  class { '::traefik2':
+    dynamic_config => $dynamic_config,
+    version        => $version,
+    static_config  => $static_config,
+  }
+
+  if $manage_firewall_entry {
+    profiles::bootstrap::firewall::entry { '200 allow Traefik HTTP and HTTPS':
+      port => $firewall_ports,
+    }
+  }
+
+  if $expose_api {
+    if $expose_ui {
+      if $manage_firewall_entry {
+        profiles::bootstrap::firewall::entry { '200 allow Traefik Proxy and API/Dashboard':
+          port => [8080],
+        }
+      }
+      if $manage_sd_service {
+        ::profiles::orchestration::consul::service { $sd_service_name:
+          checks => [
+            {
+              http     => "http://${::ipaddress}:8080",
+              interval => '10s'
+            }
+          ],
+          port   => 8080,
+          tags   => $sd_service_tags,
+        }
+      }
+    }
+  }
+
+  #if $expose_metrics {}
+}
