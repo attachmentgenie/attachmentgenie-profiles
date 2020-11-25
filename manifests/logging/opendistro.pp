642edb2dd6934a59b1f6649ea675c33e5774bf6a
Bram Vogelaar <bram@attachmentgenie.com>
2020-11-25 16:00:18 +0100

    removing references to opendistro

--- manifests/logging/opendistro.pp
+++
@@ -1,6 +0,0 @@
-# This class can be used install opendistro components.
-#
-# @example when declaring the opendistro class
-#  class { '::profiles::logging::opendistro': }
-#
-class profiles::logging::opendistro {}
