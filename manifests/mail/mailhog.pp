a854dc71c048b62bdc9501cd322c3690bec6f124
Bram Vogelaar <bram@attachmentgenie.com>
2021-04-16 00:55:21 +0200

    adding option to pass config to mailhog

--- manifests/mail/mailhog.pp
+++ manifests/mail/mailhog.pp
@@ -6,10 +6,12 @@
 # @param install_method How to install.
 # @param wget_source    Location to download mailhog.
 class profiles::mail::mailhog (
+  String $config = '-ui-bind-addr=127.0.0.1:8025 -api-bind-addr=127.0.0.1:8025',
   String $install_method = 'package',
-  Optional[String] $archive_source = undef,
+  String $archive_source = 'https://github.com/mailhog/MailHog/releases/download/v1.0.1/MailHog_linux_amd64',
 ){
   class { '::mailhog':
+    config         => $config,
     install_method => $install_method,
     archive_source => $archive_source,
   }
