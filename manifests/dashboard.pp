4cd79b584f7da3747d2e37d6153e32f1daea0d87
Bram Vogelaar <bram@attachmentgenie.com>
2021-05-16 02:55:26 +0200

    initial setup of statping

--- manifests/dashboard.pp
+++ manifests/dashboard.pp
@@ -6,13 +6,17 @@
 # @param smashing Manage smashing on this node.
 # @param icinga2  Mange icinga2web on this node.
 class profiles::dashboard (
-  Boolean $smashing = false,
   Boolean $icinga2 = false,
+  Boolean $smashing = false,
+  Boolean $statping = false,
 ) {
+  if $icinga2 {
+    class { '::profiles::dashboard::icinga2': }
+  }
   if $smashing {
     class { '::profiles::dashboard::smashing': }
   }
-  if $icinga2 {
-    class { '::profiles::dashboard::icinga2': }
+  if $statping {
+    class { '::profiles::dashboard::statping': }
   }
 }
