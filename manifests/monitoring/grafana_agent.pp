c405bc5c99f77fc94753517cdbc98ba1e2a83fb0
Bram Vogelaar <bram@attachmentgenie.com>
2021-09-08 22:14:35 +0200

    bumping version of relevant obsevability programs, introducing grafana-agent

---
+++ manifests/monitoring/grafana_agent.pp
@@ -0,0 +1,9 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include profiles::monitoring::grafana_agent
+class profiles::monitoring::grafana_agent {
+  class { 'grafana_agent': }
+}
